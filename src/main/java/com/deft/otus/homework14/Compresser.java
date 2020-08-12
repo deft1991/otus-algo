49f461b0dd0a8f20e3c98adfa5bd7ee599dff988
sgolitsyn <pt.sgolitsyn@prosper.com>
2020-08-12 14:59:23 +0300

    Homework14 Clean code

--- src/main/java/com/deft/otus/homework14/Compresser.java
+++ src/main/java/com/deft/otus/homework14/Compresser.java
@@ -2,14 +2,12 @@ package com.deft.otus.homework14;
 
 import com.deft.otus.util.HomeWorkFileUtil;
 
-import java.util.function.Function;
-
 /*
  * Created by sgolitsyn on 8/12/20
  */
 public class Compresser {
 
-//    public static final String FILE_PATH_READ = "./testFiles/14.compress/for_compress/1_2_3.txt";
+    //    public static final String FILE_PATH_READ = "./testFiles/14.compress/for_compress/1_2_3.txt";
 //    public static final String FILE_PATH_READ = "./testFiles/14.compress/for_compress/3_3_3.txt";
 //    public static final String FILE_PATH_READ = "./testFiles/14.compress/for_compress/111-medium.txt";
 //    public static final String FILE_PATH_READ = "./testFiles/14.compress/for_compress/111-large.txt";
@@ -31,23 +29,14 @@ public class Compresser {
     }
 
     public void RLECompressAndWrite(String FILE_PATH_READ, String FILE_PATH_WRITE, String FILE_WRITE_NAME) {
-        byte[] bytes = compressFile(new RLEFunction().rleCompression(), FILE_PATH_READ);
-        HomeWorkFileUtil.writeFile(FILE_PATH_WRITE + FILE_WRITE_NAME, bytes);
+        byte[] bytes = HomeWorkFileUtil.readFileToByteArray(FILE_PATH_READ);
+        byte[] bytesCompressed = new RLEFunction().rleCompression().apply(bytes);
+        HomeWorkFileUtil.writeFile(FILE_PATH_WRITE + FILE_WRITE_NAME, bytesCompressed);
     }
 
     public void RLEDeCompressAndWrite(String FILE_PATH_READ, String FILE_PATH_WRITE, String FILE_WRITE_NAME) {
-        Byte[] bytes = deCompressFile(new RLEFunction().rleDeCompression(), FILE_PATH_READ);
-        HomeWorkFileUtil.writeFile(FILE_PATH_WRITE + FILE_WRITE_NAME, bytes);
-    }
-
-
-    private byte[] compressFile(Function<byte[], byte[]> functionCompress, String FILE_PATH_READ) {
         byte[] bytes = HomeWorkFileUtil.readFileToByteArray(FILE_PATH_READ);
-        return functionCompress.apply(bytes);
-    }
-
-    private Byte[] deCompressFile(Function<byte[], Byte[]> functionDeCompress, String FILE_PATH_READ) {
-        byte[] bytes = HomeWorkFileUtil.readFileToByteArray(FILE_PATH_READ);
-        return functionDeCompress.apply(bytes);
+        Byte[] bytesDecompressed = new RLEFunction().rleDeCompression().apply(bytes);
+        HomeWorkFileUtil.writeFile(FILE_PATH_WRITE + FILE_WRITE_NAME, bytesDecompressed);
     }
 }
